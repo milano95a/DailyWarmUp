d1719260c582e5ffbd969e1654072752af8d6e07
Abdurakhmon <abdur@basemap.com>
2023-02-14 13:20:08 +0500

    Initial Commit

---
+++ DailyWarmUp/DailyWarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWarmUpApp.swift
+//  DailyWarmUp
+//
+//  Created by Workspace on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyWarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
